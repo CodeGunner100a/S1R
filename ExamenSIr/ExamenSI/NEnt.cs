87e2dc05855e8557ba11a431cc38928b43328503
CodeGunner100a <codegunner178@gmail.com>
2026-05-13 07:47:36 -0400

    Add files via upload

---
+++ ExamenSIr/ExamenSI/NEnt.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenSI
+{
+    class NEnt
+    {
+        private int n;
+        //Contructor
+        public NEnt()
+        {
+            n = 0;
+        }
+
+
+        public void Cargar(int dato)
+        {
+            n = dato;//5
+        }
+
+        public int Descargar()
+        {
+            return n;
+        }
+
+        public bool VerifPrimo()
+        {
+            int c = 0;
+            bool ban = true;
+            //              6 <= 5
+            for (int i = 1; i <= n; i++)
+            {
+                //  5 / 5 == 0 ?
+                if (n % i == 0)
+                {
+                    c = c + 1;//2    (si)
+                }
+
+            }
+
+            if (c == 2)
+            {
+                ban = true;
+            }
+            else
+            {
+                ban = false;
+            }
+
+
+            return ban;
+
+        }
+
+    }
+}
