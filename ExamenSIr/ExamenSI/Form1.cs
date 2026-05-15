ea38c01af979b68d7e4710564031b8dff73e2176
CodeGunner100a <codegunner178@gmail.com>
2026-05-15 17:07:36 -0400

    Add files via upload

---
+++ ExamenSIr/ExamenSI/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamenSI
+{
+    public partial class Form1 : Form
+    {
+        Vector v1, v2, v3, v4;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            v1 = new Vector();
+            v2 = new Vector();
+            v3 = new Vector();
+            v4 = new Vector();
+
+        }
+
+        //cargar 1
+        private void cargarRandomToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            v1.CargarRnd(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text));
+            textBox4.Text = v1.Descargar();
+        }
+
+        private void descargarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox5.Text = v1.Descargar();
+        }
+
+
+    
+
+  
+
+        //Examen
+        private void pregunta1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //1
+            int a = int.Parse(textBox2.Text);
+            int b = int.Parse(textBox3.Text);
+
+            v1.Ejercicio1(a, b);
+
+            //textBox6.Text = v1.Descargar();
+        }
+
+        private void pregunta2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //2
+            int a = int.Parse(textBox2.Text);
+            int b = int.Parse(textBox3.Text);
+
+            v1.Ejercicio2(a, b, ref v2, ref v3);
+
+            //textBox6.Text = v2.Descargar(); // elementos
+            //textBox7.Text = v3.Descargar(); // frecuencias
+        }
+
+    }
+}
---
+++ ExamenSIr/ExamenSI/Vector.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenSI
+{
+    class Vector
+    {
+
+        const int MAX = 50;
+        private int[] v;
+        private int n;
+
+
+
+        public Vector()
+        {
+            n = 0;
+            v = new int[MAX];
+
+        }
+
+
+        public void CargarRnd(int n1, int a, int b)
+        {
+
+            Random r = new Random();
+
+            n = n1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                v[i] = r.Next(a, b);
+            }
+        }
+
+
+
+
+        public string Descargar()
+        {
+            string s = "";
+
+            for (int i = 1; i <= n; i++)
+            {
+
+                s = s + v[i] + "  |  ";
+            }
+
+            return s;
+        }
+
+        
+
+        //Ejercicio 1
+        public void Ejercicio1(int a, int b)
+        {
+            NEnt n1 = new NEnt();
+
+            int i = a;
+
+            while (i <= b)
+            {
+                n1.Cargar(v[i]);
+
+                if (n1.VerifPrimo())
+                {
+                    EliminarPos(i);
+                    b--; 
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+        public void EliminarPos(int pos)
+        {
+            for (int i = pos; i < n; i++)
+            {
+                v[i] = v[i + 1];
+            }
+
+            n--;
+        }
+
+
+        //Ejercicio 2
+
+        public void Ejercicio2(int a, int b, ref Vector e, ref Vector f)
+        {
+            e.n = 0;
+            f.n = 0;
+
+            NEnt n1 = new NEnt();
+
+            for (int i = a; i <= b; i++)
+            {
+                n1.Cargar(v[i]);
+
+               
+                if (n1.VerifPrimo())
+                {
+                    
+                    if (!e.Existe_elemento(v[i]))
+                    {
+                        e.insertar(v[i]);
+
+                        int frec = frecuencia_rango(v[i], a, b);
+                        f.insertar(frec);
+                    }
+                }
+            }
+
+       
+        }
+
+        public int frecuencia_rango(int ele, int a, int b)
+        {
+            int cont = 0;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (v[i] == ele)
+                {
+                    cont++;
+                }
+            }
+
+            return cont;
+        }
+        public void insertar(int ele)
+        {
+            n = n + 1;
+            v[n] = ele;
+
+        }
+        public bool Existe_elemento(int ele)
+        {
+            int i = 1;
+            bool ban = false;
+
+            while ((i <= n) && (ban == false))
+            {
+                if (v[i] == ele)
+                {
+                    ban = true;
+                }
+                else
+                {
+                    i = i + 1;
+                }
+
+            }
+
+            return ban;
+        }
+
+    }
+}
